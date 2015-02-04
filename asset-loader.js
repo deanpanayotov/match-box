5df1b974fe134f6c3f5b50d53b00ea90b69f2d18
Dean Panayotov <deanpanayotov@gmail.com>
2015-02-05 01:31:46 +0200

    Added asset-loader script; Added wall images. Created assets folder.

---
+++ asset-loader.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Dean Panayotov Local on 4.2.2015 г..
+ */
+var a_i_wall_wh_v = new Image();
+a_i_wall_wh_v.src = 'assets/image/wall_wh_v.png';
+var a_i_wall_wh_h = new Image();
+a_i_wall_wh_h.src = 'assets/image/wall_wh_h.png';
+var a_i_wall_wh_x = new Image();
+a_i_wall_wh_x.src = 'assets/image/wall_wh_x.png';
