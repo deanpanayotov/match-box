a2b7bf8cd96a6e52748e0ededaf0b702c8a3cbc9
Dean Panayotov <deanpanayotov@gmail.com>
2015-02-05 04:14:20 +0200

    Changed image names.

--- asset-loader.js
+++ asset-loader.js
@@ -3,7 +3,7 @@
  */
 var a_i_wall_wh_v = new Image();
 a_i_wall_wh_v.src = 'assets/images/wall_wh_v.png';
-var a_i_wall_wh_h = new Image();
+var a_i_wall_wh_h = new Image(); 
 a_i_wall_wh_h.src = 'assets/images/wall_wh_h.png';
 var a_i_wall_wh_x = new Image();
 a_i_wall_wh_x.src = 'assets/images/wall_wh_x.png';
