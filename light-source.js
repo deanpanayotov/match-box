2a49117e36dd91dd7c8fde01d3e8511254a5b8be
Dean Panayotov <deanpanayotov@gmail.com>
2015-02-15 21:18:32 +0200

    LightSourceManager moved to a separate file.

--- light-source.js
+++ light-source.js
@@ -2,43 +2,6 @@
  * Created by Dean Panayotov Local on 11.2.2015 г..
  */
 
-var LightSourceManager = function () {
-
-    this.lightSources = [];
-    this.idCounter = 0;
-
-    this.update = function () {
-        for (var i = 0; i < this.lightSources.length; i++) {
-            if (this.lightSources[i]) {
-                this.lightSources[i].update();
-            }
-        }
-    }
-
-    setInterval((function(self) { return function() { self.update(); } })(this), LightSourceManager.INTERVAL);
-
-
-    this.addLightSource = function (ls) {
-        this.lightSources[this.idCounter] = ls;
-        return this.idCounter++;
-    }
-
-    this.removeLightSource = function (id) {
-        this.lightSources[id] = undefined;
-    }
-
-    this.renderLayer = function (ctx, i) {
-        for (var j = 0; j < this.lightSources.length; j++) {
-            if (this.lightSources[j]) {
-                this.lightSources[j].render(ctx, i);
-            }
-        }
-    }
-}
-
-LightSourceManager.INTERVAL = 120;
-LightSourceManager.LIGHT_LAYERS = 3;
-
 var LightSource = function (x, y, r, rflicker) {
     this.x = x;
     this.y = y;
